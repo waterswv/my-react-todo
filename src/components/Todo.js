adfc536db020c74f0e455b403669dbe457137dd1
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-08-02 15:20:30 -0700

    completed sprint 3

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+// src/components/Todo.js
+import React, {Component} from 'react'
+
+class Todo extends Component {
+  render(){
+    return(
+      <p data-todos-index={this.props.todo._id}>
+        <span>{this.props.todo.body}</span>
+      </p>
+    )
+  }
+}
+
+export default Todo
---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+// src/components/TodoList.js
+import React, {Component} from 'react'
+import Todo from './Todo'
+
+class TodoList extends Component {
+  render(){
+    let todoArray = this.props.todos.map( (todo) => {
+      return (
+        <Todo
+          key={todo._id}
+          todo={todo}/>
+      )
+    })
+    return(
+      <div className="todos">
+        {todoArray}
+      </div>
+    )
+  }
+}
+
+export default TodoList
