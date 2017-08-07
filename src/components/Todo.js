132af2913f96557d7c29259c0d12a75f98c9a691
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-08-07 11:43:47 -0700

    completed sprint 5

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,21 @@
 // src/components/Todo.js
 import React, {Component} from 'react'
+import EditTodoForm from '../components/EditTodoForm'
 
 class Todo extends Component {
   render(){
     return(
-      <p data-todos-index={this.props.todo._id}>
-        <span>{this.props.todo.body}</span>
-          <span
-            className='deleteButton'
-            onClick={() => this.props.onDeleteTodo(this.props.todo)}>
-              (X)
-          </span>
-      </p>
+      <div>
+        <p data-todos-index={this.props.todo._id}>
+          <span>{this.props.todo.body}</span>
+            <span
+              className='deleteButton'
+              onClick={() => this.props.onDeleteTodo(this.props.todo)}>
+                (X)
+            </span>
+        </p>
+        <EditTodoForm />
+      </div>
     )
   }
 }
